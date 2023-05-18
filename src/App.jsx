b7ce9dcc07a16ccd8a1ef574e1d20ace6f2abf8a
Yamilet Jerez <quieromiwww@gmail.com>
2023-05-18 21:38:15 +0200

    primer commit portfolio

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="container md:container md:mx-auto">
+       <h1 className="text-3xl font-bold text-center">
+      Hello portfolio Yami!
+    </h1>
+    </div>
+   
+  )
+}
+export default App;
