18653cf2f1a58f0469b82f82a66bf2e7e8cf89f4
Yamilet Jerez <quieromiwww@gmail.com>
2023-05-30 19:47:56 +0200

    -foto

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ const App = () => {
     md:bg-[url('./assets/images/bg-desktop-dark.jpg')] md:dark:bg-[url('./assets/images/bg-desktop-light.jpg')]">
     <Header />
         <div>
+          
           <h1 className="header text-3xl font-bold text-center pb-2 pt-4 text-white">
             Hola soy Yami!
           </h1>
@@ -15,9 +16,11 @@ const App = () => {
         </div>
        
         <div className="container md:container">
+          
         <div className="grid grid-cols-1 gap-2">
 
           <div className="grid grid-rows-6 lg:grid-rows-2 grid-flow-col gap-2 sm:grid-rows-6">
+            
           <div className="col-end-6 col-span-6 bg-teal-200 lg:h-52 sm:col-span-4 sm:h-1/2">
             <h2 className="font-bold pl-4 pb-2 pt-4 text-left">
                 Diseñador gráfico
@@ -49,24 +52,9 @@ const App = () => {
           
     </div>
 
-    <strong>otros</strong>
-
-    <div className="container md:container md:mx-auto">
-          {/*  <div className="grid lg:grid-cols-6 gap-2 sm:grid-cols-3"> */}
-            <div className="grid grid-rows-4 grid-flow-col gap-2">
-            <div className="col-span-2 bg-teal-200 lg:h-52 lg:col-span-2 sm:col-span-6 sm:col-start-1 sm:col-end-7 sm:h-1/2">01</div>
-
-            
-            <div className="col-span-1 bg-pink-100 h-32">02 </div>
-            <div className="col-span-2 bg-pink-100 h-32">02 </div>
-
-            <div className="col-span-2 bg-teal-700 h-32">03</div>
-            </div>
+    <div className="min-h-screen rounded-full bg-clip-content bg-contain bg-no-repeat bg-[url('./assets/images/yami1-1.jpg')] w-3/12 max-w-xs">
           
-          {/*  </div> */}
-    </div>
-
-   
+          </div>
       
 </div>
   )
