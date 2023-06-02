4ac6f2a624b533251e1a280a8483a5caad77323e
Yamilet Jerez <quieromiwww@gmail.com>
2023-06-02 15:32:03 +0200

    -imagen

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
           
         <div className="grid grid-cols-1 gap-2">
 
-          <div className="grid grid-rows-6 lg:grid-rows-2 grid-flow-col gap-2 sm:grid-rows-6">
+          <div className="grid grid-rows-5 lg:grid-rows-2 grid-flow-col gap-2 sm:grid-rows-6">
             
           <div className="col-end-6 col-span-6 bg-teal-200 lg:h-52 sm:col-span-4 sm:h-1/2">
             <h2 className="font-bold pl-4 pb-2 pt-4 text-left">
@@ -51,11 +51,13 @@ const App = () => {
         </div>
           
     </div>
-
-    <div className="min-h-screen rounded-full bg-clip-content bg-contain bg-no-repeat bg-[url('./assets/images/yami1-1.jpg')] w-3/12 max-w-xs">
+    <div class="grid grid-cols-1 gap-4">
+      <div className="w-3/4 h-1/2 content-center">
+    <div className="col-start-1 col-end-7 h-96 min-h-full rounded-full bg-cover bg-no-repeat bg-[url('./assets/images/yami1-1.jpg')]">
           
+          </div> 
           </div>
-      
+      </div>
 </div>
   )
 }
