bd9ca3ff610e138ec3f4b09bb329c3e39492e146
Yamilet Jerez <quieromiwww@gmail.com>
2023-09-05 15:33:47 +0200

    -

--- src/App.jsx
+++ src/App.jsx
@@ -26,8 +26,8 @@ const App = () => {
         <div className="grid grid-cols-1 gap-1 lg:grid-cols-3 mb-1 -mt-8">
         <div className="grid grid-rows-1 grid-flow-col gap-2">
               <div className="bg-teal-200 p-6">
-              <SubTitle subtitle="Diseño gráfico" />
-              <p className="text-center">Diseño de Logos, Banners, Infografías, Vectorización de imágenes, Texturas {/* <Button text="Ver más" /> */}</p>
+              {/* <SubTitle subtitle="Desarrollo web" /> */}
+                  <p className="text-center pt-4">Diseño de interfaces web a través de lenguajes de marca y lenguajes de programación, desde la arquitectura hasta el funcionamiento. Licenciada en Comunicación audiovisual con diplomatura en desarrollo web, en continuo aprendizaje como es lógico en mi área. Partiendo del diseño gráfico, maquetación con diferentes herramientas y desarrollo mediante el uso de Frameworks además Wordpress y otros CMS.</p>
               </div>
           </div>
 
@@ -37,16 +37,19 @@ const App = () => {
                 <p className="text-center">Dentro de las competencias adquiridas en mi carrera se encuentran la fotografía, edición de videos, guiones para radio cine y tv, locución de radio y el manejo de programas de edición entre los que destaca Adobe Premiere, After Effects</p>
                   </div>
                   <div className="col-span-2 col-end-4 gap-2 bg-violet-200 p-6">
-                  <SubTitle subtitle="Desarrollo web" />
-                  <p className="text-center">Desarrollo de páginas web mediante el uso de HTML, CSS, Php, Javascript y uso de Frameworks tales como React, Laravel o el diseño en CMS como Wordpress o Drupal</p>
+                  <SubTitle subtitle="Diseño web" />
+                    <p className="text-center">Destreza en el uso de herramientas de diseño y maquetación de la suite de Adobe tales como Illustrator, Photoshop así como también Figma, Canva </p>
+                    <SubTitle subtitle="Diseño gráfico" />
+                    <p className="text-center">Diseño de Logos, Banners, Infografías, Vectorización de imágenes, Texturas {/* <Button text="Ver más" /> */}</p>
+                  
                   </div>
           </div>
 
 
           <div className="grid grid-rows-1 grid-flow-col gap-2">   
             <div className="bg-teal-200 p-6">
-            <SubTitle subtitle="Diseño web" />
-            <p className="text-center">Destreza en el uso de herramientas de diseño y maquetación de la suite de Adobe tales como Illustrator, Photoshop así como también Figma, Canva </p>
+            <SubTitle subtitle="Desarrollo web" />
+                  <p className="text-center">Desarrollo de páginas web mediante el uso de HTML, CSS, JAVASCRIPT, PHP, MSQL, Frameworks como React, Laravel o con CMS como Wordpress, Drupal...</p>
               </div> 
           </div>  
 
@@ -56,14 +59,22 @@ const App = () => {
       <div className="grid grid-cols-4 gap-1 lg:grid-cols-3">
           
                 <div className="col-span-2 lg:col-end-4 bg-cyan-200 p-6">
-                  <p className="pl-4 pb-4 pt-4 text-center">
-                  Uso herramientas de la suite de Adobe para el retoque fotográfico, diseño de logos, botones para las páginas web, gif
+                <SubTitle subtitle="Experiencia" />
+                <span>2019-2023</span>
+                  <p className="text-2xl pl-4 pb-4 pt-4 text-center">
+                      Dynamic Opengov Thecnologies
                     </p>
+                    <p className="text-center">En Ogov realizo actividades vinculadas a la creación diseño y desarrollo front end, maquetación visual, 
+                      mantenimiento y optimización de sitios web e interfaces de usuario, implemento prácticas que incluyen estructuras, 
+                      bases de datos, adaptabilidad, SEO Onpage así como también de accesibilidad, además de la gestión de incidencias del mismo tipo.</p>
                   </div>
                   <div className="col-span-2 lg:col-start-2 bg-violet-200 p-6">
-                  <p className="pl-4 pb-2 pt-4 text-center">
-                  Una destreza añadida consiste en el desarrollo de páginas web mediante el uso de HTML, CSS, JAVASCRIPT, MSQL junto a otras herramientas...
+                  <span>2017-2023</span>
+                  <p className="text-2xl pl-4 pb-4 pt-4 text-center">
+                      Gomezlee Marketing
                     </p>
+                    <p className="text-center">En Gomezlee Marketing realizo actividades vinculadas a la creación diseño y desarrollo front end enfocado al marketing. 
+                    Landing pages para conversión mantenimiento y optimización de sitios web de grandes marcas.</p>
                   </div>
           
 </div>
