87d87ae2d2d937bdbe7419a712028032a1425774
Yamilet Jerez <quieromiwww@gmail.com>
2023-10-30 13:31:42 +0100

    -

--- src/App.jsx
+++ src/App.jsx
@@ -55,7 +55,7 @@ const App = () => {
 
       
       <div className="grid grid-cols-4 gap-1 lg:grid-cols-3 text-center">
-              <div className="col-span-2 lg:col-end-4 bg-gray-900 p-6 pt-4">
+              <div className="col-span-4 lg:col-end-4 bg-gray-900 p-6 pt-4">
               <div className="w-24 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-opacity-60 -mt-14 py-4 float-right shadow-md">
                     <strong className="text-4xl text-center pt-4">5</strong><small>años de<br/>experiencia</small>
                   </div>
