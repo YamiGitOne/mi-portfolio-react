f9390deb481615526161cd14a2f0d2c429ca5ebd
Yamilet Jerez <quieromiwww@gmail.com>
2023-10-30 12:17:34 +0100

    -

--- src/components/SubTitle.jsx
+++ src/components/SubTitle.jsx
@@ -1,7 +1,7 @@
 const SubTitle = ({subtitle}) => {
   return (
     <>
-    <h2 className="font-bold pl-2 pb-2 pt-4 text-center">
+    <h2 className="font-bold pl-2 pb-2 pt-4 text-center lg:text-2xl">
                 {subtitle}
               </h2>
     </>
