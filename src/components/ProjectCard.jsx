676283f6e01187c1bdc9dd54ca42e1bb2d3c43bd
Yamilet Jerez <quieromiwww@gmail.com>
2023-11-21 10:23:28 +0100

    -

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,7 +1,8 @@
 const ProjectCard = ({ title, description, imageUrl }) => {
     return (
-      <div className="max-w-xl rounded overflow-hidden shadow-lg">
-        <img className="w-full h-48 object-cover" src={imageUrl} alt={title} />
+      <div className="lg:w-1/2 max-w-xl rounded overflow-hidden shadow-lg">
+        <img className="w-full h-64 object-cover" src={`/images/projects/${imageUrl}.png`}
+  alt={title} />
   
         <div className="px-6 py-4">
           <div className="font-bold text-xl mb-2">{title}</div>
--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -3,37 +3,81 @@ import ProjectCard from "./ProjectCard";
 const projectsData = [
     {
     id: 1,
-    title: "React Porfolio Website",
-    description: "Project 1 description",
-    image: "/public/images/projects/1.png",
+    title: "Gómez Lee Marketing",
+    description: "Web Site institucional, responsive, diseñado en Wordpress. ",
+    image: "GLM",
     tag: ["All", "Web"],
     },
     {
     id: 2,
-    title: "React Porfolio Website",
-    description: "Project 1 description",
-    image: "/images/projects/1.png",
+    title: "Vozez",
+    description: "Project 2 description",
+    image: "vozez.png",
     tag: ["All", "Web"],
     },
     {
     id: 3,
-    title: "React Porfolio Website",
-    description: "Project 1 description",
-    image: "/images/projects/1.png",
+    title: "oGov",
+    description: "Project 3 description",
+    image: "oGov.png",
     tag: ["All", "Web"],
     },
     {
     id: 4,
-    title: "React Porfolio Website",
-    description: "Project 1 description",
-    image: "/images/projects/1.png",
+    title: "Transparencia",
+    description: "Project 4 description",
+    image: "pt.png",
     tag: ["All", "Web"],
     },
+    {
+    id: 5,
+    title: "Obras",
+    description: "Project 5 description",
+    image: "pt.png",
+    tag: ["All", "Web"],
+    },
+    {
+    id: 6,
+    title: "Whirpooll",
+    description: "Project 6 description",
+    image: "whirpooll.png",
+    tag: ["All", "Web"],
+    },
+    {
+    id: 7,
+    title: "Arena gorda",
+    description: "Project 7 description",
+    image: "constructora.png",
+    tag: ["All", "Web"],
+    },
+    {
+    id: 8,
+    title: "Prestashop",
+    description: "Project 8 description",
+    image: "prestashop.png",
+    tag: ["All", "Web"],
+    },
+    {
+    id: 9,
+    title: "Suscription site",
+    description: "Project 9 description",
+    image: "suscription.png",
+    tag: ["All", "Web"],
+    },
+    {
+    id: 10,
+    title: "API clientes",
+    description: "Project 10 description",
+    image: "clientes.png",
+    tag: ["All", "Web"],
+    }
 ]
 const ProjectsSection = () => {
     return(
-        <div>
-            {projectsData.map((project) => <ProjectCard key={project.id} title={project.title} description={project.description} imgUrl={project.image} />)}
+        <div className="md:container md:mx-auto">
+        <div className="lg:inline-flex lg:flex-wrap ">
+            {projectsData.map((projects) => <ProjectCard key={projects.id} title={projects.title} description={projects.description} imageUrl={projects.image} />)}
+        </div>
         </div>
     )
 }
