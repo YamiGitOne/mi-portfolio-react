182bcff1d73a85eab0e5b9a1688bbe60f17ae53f
Yamilet Jerez <quieromiwww@gmail.com>
2023-11-23 11:51:17 +0100

    -

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import {FaBars} from "react-icons/fa"
+import {FaBars} from "react-icons/fa";
 const NavBar = () => {
     let Links =[
         {name:"Inicio", link:"/"},
